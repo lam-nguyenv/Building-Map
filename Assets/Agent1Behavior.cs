ba262a0d9aa56d907c13c17f0fac0e83f6c10460
lamnguyen <43162568+lam-nguyenv@users.noreply.github.com>
2018-10-08 22:44:01 +0700

    Test

--- Assets/Agent1Behavior.cs
+++ Assets/Agent1Behavior.cs
@@ -7,10 +7,12 @@ public class Agent1Behavior : MonoBehaviour {
     public float speed;
     public float moveHorizontal;
     public float moveVertical;
+    public Agent2Behavior agent2;
+
     // Use this for initialization
     void Start () {
         rb2d = GetComponent<Rigidbody2D>();
-        
+        agent2 = GetComponent<Agent2Behavior>();
     }
 	
 	// Update is called once per frame
@@ -19,8 +21,11 @@ public class Agent1Behavior : MonoBehaviour {
         moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
         rb2d.AddForce (movement * speed);
-        Vector2 agent2Position = new Vector2();
-        agent2Position = Agent2Behavior.getPosition();
+
+        Vector2 agent2Position = new Vector2(0, 0);
+        agent2Position = agent2.Update();
+        Debug.Log("Agent 2 Position: " + agent2Position);
+
     }
 
 
--- Assets/Agent2Behavior.cs
+++ Assets/Agent2Behavior.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Agent2Behavior : MonoBehaviour {
+    public Vector2 position;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-    }
 
-    public Vector2 getPosition()
-    {
+    // Update is called once per frame
+    public Vector2 Update () {
         return this.transform.position;
     }
+
+
 }

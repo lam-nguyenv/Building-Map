830d5b2e352edcbaf362ab5b1e158ca381263e47
lamnguyen <43162568+lam-nguyenv@users.noreply.github.com>
2018-10-08 16:40:20 +0700

    Initial commit

---
+++ Assets/Agent1Behavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent1Behavior : MonoBehaviour {
+    private Rigidbody2D rb2d;
+    public float speed;
+    public float moveHorizontal;
+    public float moveVertical;
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+        rb2d.AddForce (movement * speed);
+        Vector2 agent2Position = new Vector2();
+        agent2Position = Agent2Behavior.getPosition();
+    }
+
+
+}
---
+++ Assets/Agent2Behavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent2Behavior : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    public Vector2 getPosition()
+    {
+        return this.transform.position;
+    }
+}
